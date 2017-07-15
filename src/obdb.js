fa82cbfaa1ab8984ab90873313374dd4a6035f06
Zane Hitchcox <zwhitchcox@gmail.com>
2017-07-15 14:57:25 -0400

    converting to remote first

--- src/obdb.js
+++ src/obdb.js
@@ -1,95 +1,104 @@
 import global from './global'
 import uuid from 'uuid/v4'
+import { isPlainObject } from './util'
+import io from 'socket.io-client'
 
+const MAX_ITERATIONS = 100
 const noop = () => {}
 
-export default class Obdb {
-  values = {}
-  curIterations = 0
-  reactionsToProps = {}
-  propsToReactions = {}
-  pendingReactions =  []
-  inReaction =  false
-  reportObserved = noop
-  reactions = {}
+const reactionsToProps = {}
+const propsToReactions = {}
+const reactions = {}
 
-  constructor(options) {
-    Object.assign(this, options, {
-      peers: [],
-      maxIterations: 100,
-    })
-    this.mkob = this.makeObservable.bind(this)
-    this.aur = this.autorun.bind(this)
+let curIterations = 0
+let pendingReactions =  []
+let inReaction = false
+let reportObserved = noop
+
+export default function Obdb(options = {}) {
+  options.peers = [].concat(options.peers)
+  Object.assign(options, {})
+  const returnFn = makeObservable()
+  return {
+    mkob: makeObservable
   }
+ }
 
-  makeObservable(obj) {
-    const that = this
-    for (const key in obj) {
-      const propId = uuid()
-      that.values[propId] = obj[key]
-      Object.defineProperty(obj, key, {
-        get() {
-          that.reportObserved(propId)
-          return that.values[propId]
-        },
-        set(val) {
-          that.values[propId] = val
-          that.reportObserved(propId)
-          that.triggerReactions(propId)
-          return val
-        }
-      })
-    }
-  }
-
-  autorun(cb) {
-    const reactionId = uuid()
-    this.reactions[reactionId] = cb
-    this.observe(reactionId)
-    return () => {
-      this.clearReactions(reactionId)
-      delete this.reactions[reactionId]
-    }
-  }
-
-  triggerReactions(propId) {
-    this.curIterations++
-    this.pendingReactions.push(propId)
-    if (this.inReaction) return
-    const newReactions = this.pendingReactions.slice()
-    this.pendingReactions = []
-    this.curIterations = 0
-    this.inReaction = true
-    do {
-      if (this.curIterations > this.MAX_ITERATIONS)
-        throw new Error('Looks like there\'s an infinite loop'
-          + 'in your code, as this iterations has triggered over 100 other iterations')
-
-      for (let i = 0; i < newReactions.length; i++)  {
-        const curReactions = this.propsToReactions[newReactions[i]]
-        for (const curReaction in curReactions) {
-          this.clearReactions(curReaction)
-          this.observe(curReaction)
-        }
+export { makeObservable as mkob }
+export makeObservable = ({peers = [], delimiter = '.'}) => path => obj => {
+  peers = [].concat(peers)
+  const connections = peers.map(io)
+  connections.forEach(socket => socket.on('connect', () => console.log('connected')))
+  path = (typeof path === string) ? 
+    path.split(delimiter) : path
+  for (const key in obj) {
+    let val;
+    const propId = uuid()
+    val = obj[key]
+    Object.defineProperty(obj, key, {
+      get() {
+        reportObserved(propId)
+        return val
+      },
+      set(newVal) {
+        val = newVal
+        reportObserved(propId)
+        triggerReactions(propId)
+        return val
       }
-    } while (this.pendingReactions.length)
-    this.inReaction  = false
+    })
   }
+}
 
-  clearReactions(reactionId) {
-    for (const propId in this.reactionsToProps[reactionId]) {
-      delete this.propsToReactions[propId][reactionId]
-    }
-    delete this.propsToReactions[reactionId]
+export { autorun as aur }
+export function autorun(cb) {
+  const reactionId = uuid()
+  reactions[reactionId] = cb
+  observe(reactionId)
+  return () => {
+    clearReactions(reactionId)
+    delete reactions[reactionId]
   }
-  observe(reactionId) {
-    let prevReportObserved = this.reportObserved
-    this.reportObserved = propId => {
-      ;(this.propsToReactions[propId] || (this.propsToReactions[propId] = {}))[reactionId] = true
-      ;(this.reactionsToProps[reactionId] || (this.reactionsToProps[reactionId] = {}))[propId] = true
-      prevReportObserved(propId)
+}
+
+export function triggerReactions(propId) {
+  curIterations++
+  pendingReactions.push(propId)
+  if (inReaction) return
+  const newReactions = pendingReactions.slice()
+  pendingReactions = []
+  curIterations = 0
+  inReaction = true
+  do {
+    if (curIterations > MAX_ITERATIONS)
+      throw new Error('Looks like there\'s an infinite loop'
+        + ' in your code, as this iterations has triggered over 100 other iterations')
+
+    for (let i = 0; i < newReactions.length; i++)  {
+      const curReactions = propsToReactions[newReactions[i]]
+      for (const curReaction in curReactions) {
+        clearReactions(curReaction)
+        observe(curReaction)
+      }
     }
-    this.reactions[reactionId]()
-    this.reportObserved = prevReportObserved
+  } while (pendingReactions.length)
+  inReaction  = false
+}
+
+export function clearReactions(reactionId) {
+  for (const propId in reactionsToProps[reactionId]) {
+    delete propsToReactions[propId][reactionId]
   }
+  delete propsToReactions[reactionId]
+}
+
+export function observe(reactionId) {
+  let prevReportObserved = reportObserved
+  reportObserved = propId => {
+    ;(propsToReactions[propId] || (propsToReactions[propId] = {}))[reactionId] = true
+    ;(reactionsToProps[reactionId] || (reactionsToProps[reactionId] = {}))[propId] = true
+    prevReportObserved(propId)
+  }
+  reactions[reactionId]()
+  reportObserved = prevReportObserved
 }
--- src/obdb.spec.js
+++ src/obdb.spec.js
@@ -1,24 +1,7 @@
 import Obdb from './obdb'
 import * as expect from 'expect'
 import sinon from 'sinon'
-const obdb = new Obdb
-const {
-  mkob,
-  aur,
-} = obdb;
+import { mkob, aur } from './obdb'
 
 it('should work with observers', () => {
-  const ob = { hello: 'hi' }
-  let x;
-  mkob(ob)
-  const destroyerOfWorlds = aur(() => x = ob.hello)
-  expect(x).toBe('hi')
-  ob.hello = 'no'
-  expect(x).toBe('no')
-  destroyerOfWorlds()
-  ob.hello = 'yes'
-  expect(x).toBe('no')
-})
-
-it('should mirror on database', () => {
 })
