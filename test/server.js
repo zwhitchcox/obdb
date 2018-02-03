99373961cb85ec3901bcaf736edf4ec2c129fc7f
Zane Hitchcox <zwhitchcox@gmail.com>
2018-02-02 20:04:43 -0500

    reload when ready

--- test/server.js
+++ test/server.js
@@ -1,16 +1,29 @@
-const express = require('express')
+const url = require('url')
 const WS = require('ws')
+const express = require('express')
+
+
 const app = express()
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.static(__dirname))
+app.get('/ready', (req, res) => res.end('true'))
 
 const server = app.listen(3000, () => console.log('app is listening http://localhost:3000'))
-const wss = new WS.Server({server})
+const wss = new WS.Server({noServer: true})
 
-wss.on('connection', (ws, req) => {
-
-  ws.on('message', message => {
-    console.log('rec eived %s', message)
-  })
-  ws.send('something')
+server.on('upgrade', (req, socket, head) => {
+  const pathname = url.parse(req.url, true)
+  if (pathname.href === '/pirror') {
+    wss.handleUpgrade(req, socket, head, ws => {
+      ws.on('message', message => {
+        console.log('received %s', message)
+      })
+      ws.send('something 19')
+    })
+  }
 })
--- test/server.runner.js
+++ test/server.runner.js
@@ -1,6 +1,17 @@
 const gaze = require('gaze')
 const cp = require('child_process')
 const path = require('path')
+const express = require('express')
+const app = express()
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.listen(3002)
+let ready = 'false'
+app.get('/ready', (req, res) => res.send(ready))
 
 let server;
 gaze(__dirname + '/build/server.js', (err, watcher) => {
@@ -8,11 +19,19 @@ gaze(__dirname + '/build/server.js', (err, watcher) => {
   watcher.on('all', runserver)
 })
 function runserver() {
-  console.log('restarting')
   if(server) {
+    ready = 'false'
+    console.log('restarting')
     server.kill('SIGTERM')
   }
-  server = cp.spawn('node', [__dirname + '/build/server'], {stdio: 'inherit'})
+  server = cp.spawn('node', [__dirname + '/build/server'])
+  server.stdout.on('data', data => {
+    if (/is listening/.test(data.toString())) {
+      ready = 'true'
+    }
+    console.log(data.toString())
+  })
+  server.stderr.on('data', data => console.log(data.toString()))
 }
 
 process.on('exit', () => {
@@ -22,9 +41,12 @@ process.on('exit', () => {
 })
 
 const projectroot = path.resolve(__dirname, '..')
-gaze([projectroot + '/{test,src}/{**/,}*.js', '!'+projectroot+'/test/build/**'], (err, watcher) => {
-  watcher.on('all', () => {
-    console.log('compiling')
-    cp.exec('yarn test:server:compile', {stdio: 'inherit', cwd: projectroot})
-  })
+let compiler;
+gaze([projectroot + '/{test,src}/{**/,}*', '!'+projectroot+'/test/build/**'], (err, watcher) => {
+  compile()
+  watcher.on('all', compile)
 })
+
+function compile() {
+  compiler = cp.spawn('yarn', ['test:server:compile'], {stdio: 'inherit', cwd: projectroot})
+}
