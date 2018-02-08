554b6ecee45f0bd31dad9af5d3ad431ae67c5021
Zane Hitchcox <zwhitchcox@gmail.com>
2018-02-08 16:36:36 -0500

    add index.html page

--- playground/reload.js
+++
@@ -1,7 +0,0 @@
-const ws = new WebSocket('ws://localhost:3002')
-ws.onmessage = function(msg) {
-  if (msg.data === 'refresh') {
-    location.reload()
-  }
-}
-
--- src/server.reflex.test.js
+++
@@ -1,4 +0,0 @@
-it('literally nothing', () => {
-
-
-})
