f3d5668173d70355767798aeca019e2a26301925
Zane Hitchcox <zwhitchcox@gmail.com>
2018-04-21 16:39:56 -0400

    trying to get github to update

--- playground/home.js
+++ playground/home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {observable} from 'mobx'
 import {observer} from "mobx-react"
+import { Link } from './link'
 
 @observer export default class Home extends React.Component {
 
